f5dce69be3d10c4352a0ba8bf5fa6d2c3f39660a
John Unland <unlandj2012@gmail.com>
2020-09-30 08:40:42 -0500

    Update containers.

--- containers/files/start-searx.sh
+++ containers/files/start-searx.sh
@@ -3,6 +3,6 @@
 sed -i -e "s|base_url : False|base_url : ${BASE_URL}|g" \
        -e "s/image_proxy : False/image_proxy : ${IMAGE_PROXY}/g" \
        -e "s/ultrasecretkey/$(openssl rand -hex 32)/g" \
-       /opt/local/searx/searx/settings.yml
+       /opt/searx/searx/settings.yml
 
 exec python3 /opt/searx/searx/webapp.py
