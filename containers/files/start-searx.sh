a3c9b2ea82064c3d848d587da184821286c1f88a
John Unland <unlandj2012@gmail.com>
2020-09-30 10:04:54 -0500

    Add uwsgi to searx

--- containers/files/start-searx.sh
+++ containers/files/start-searx.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+export DEFAULT_BIND_ADDRESS="0.0.0.0:8888"
+if [ -z "${BIND_ADDRESS}" ]; then
+    export BIND_ADDRESS="${DEFAULT_BIND_ADDRESS}"
+fi
+
 sed -i -e "s/base_url : False|base_url : ${BASE_URL}|g" \
        -e "s/image_proxy : False: ${IMAGE_PROXY}/g" \
        -e "s/ultrasecretkey/$(openssl rand -hex 32)/g" \
        /opt/searx/searx/settings.yml
 
-exec python3 /opt/searx/searx/webapp.py
+exec uwsgi --master --http-socket "${BIND_ADDRESS}" /etc/uwsgi.d/searx.ini
