2bcbc269de3fbe16bd0f9ff83bacf135da823ab3
John Unland <unlandj2012@gmail.com>
2020-09-30 09:02:12 -0500

    Update searx

--- containers/files/start-searx.sh
+++ containers/files/start-searx.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sed -i -e "s|base_url : False|base_url : ${BASE_URL}|g" \
-       -e "s/image_proxy : False/image_proxy : ${IMAGE_PROXY}/g" \
+sed -i -e "s/base_url : False|base_url : ${BASE_URL}|g" \
+       -e "s/image_proxy : False: ${IMAGE_PROXY}/g" \
        -e "s/ultrasecretkey/$(openssl rand -hex 32)/g" \
        /opt/searx/searx/settings.yml
 
