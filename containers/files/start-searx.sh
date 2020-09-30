f9efd948245b756655fff2a941e3b07c82a3e6e3
John Unland <unlandj2012@gmail.com>
2020-09-30 10:48:19 -0500

    Update

--- containers/files/start-searx.sh
+++ containers/files/start-searx.sh
@@ -5,8 +5,8 @@ if [ -z "${BIND_ADDRESS}" ]; then
     export BIND_ADDRESS="${DEFAULT_BIND_ADDRESS}"
 fi
 
-sed -i -e "s/base_url : False|base_url : ${BASE_URL}|g" \
-       -e "s/image_proxy : False: ${IMAGE_PROXY}/g" \
+sed -i -e "s/base_url : False : ${BASE_URL}/g" \
+       -e "s/image_proxy : False : ${IMAGE_PROXY}/g" \
        -e "s/ultrasecretkey/$(openssl rand -hex 32)/g" \
        /opt/searx/searx/settings.yml
 
