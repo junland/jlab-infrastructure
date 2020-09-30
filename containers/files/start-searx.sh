cc3ba69f7d396f3dedeabfd2848cda3f68e96887
John Unland <unlandj2012@gmail.com>
2020-09-30 10:52:47 -0500

    Add morty config

--- containers/files/start-searx.sh
+++ containers/files/start-searx.sh
@@ -11,4 +11,15 @@ sed -i -e "s|base_url : False|base_url : ${BASE_URL}|g" \
        -e "s/ultrasecretkey/$(openssl rand -hex 32)/g" \
        /opt/searx/searx/settings.yml
 
+if [ ! -z "${MORTY_KEY}" -a ! -z "${MORTY_URL}" ]; then
+	sed -i -e "s/image_proxy : False/image_proxy : True/g" /opt/searx/searx/settings.yml
+	
+       cat >> /opt/searx/searx/settings.yml <<-EOF
+# Morty configuration
+result_proxy:
+   url : ${MORTY_URL}
+   key : !!binary "${MORTY_KEY}"
+EOF
+fi
+
 exec uwsgi --master --http-socket "${BIND_ADDRESS}" /etc/uwsgi.d/searx.ini
